e08a82290c3136c3dd9244b3a3b5a2cdd93035cd
prasad ys <ys.prasad28@gmail.com>
2024-08-05 15:51:20 +0530

    r

--- backend/dist/app.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { config } from "dotenv";
-config();
-const app = express();
-export default app;
-//# sourceMappingURL=app.js.map
--- backend/dist/index.js
+++
@@ -1,7 +0,0 @@
-import app from "./app.js";
-import { connectToDatabase } from "./db/connection.js";
-const port = process.env.PORT || 5000;
-connectToDatabase()
-    .then(() => { app.listen(port, () => console.log("server open and connected to db❤️")); })
-    .catch((err) => { console.log(err); });
-//# sourceMappingURL=index.js.map
