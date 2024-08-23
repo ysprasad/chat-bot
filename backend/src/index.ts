348b8f84c7129f9126df42dc2056111d84a0172d
prasad ys <ys.prasad28@gmail.com>
2024-08-23 17:52:46 +0530

    backend done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,12 @@
 import app from "./app.js";
-import  {connectToDatabase } from "./db/connection.js";
-const port=process.env.PORT||5000
+import { connectToDatabase } from "./db/connection.js";
 
+//connections and listeneres
+const PORT = process.env.PORT || 5000;
 connectToDatabase()
-.then(()=>{app.listen(port,()=>console.log("server open and connected to db❤️"))})
-.catch((err)=>{console.log(err)});
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log("Server Open & Connected To Database 🤟")
+    );
+  })
+  .catch((err) => console.log(err));
